afe711e9e0550dc33d9d10595953e50021154a2f
Exortile <xortile@gmail.com>
2025-04-05 22:17:17 +0300

    Add support for encoding all non-palettized formats

--- src/lib.rs
+++ src/lib.rs
@@ -9,14 +9,14 @@ pub mod formats;
 /// block iterators.
 ///
 /// This macro allows adding a block of statements on each iteration of a full block, which is
-/// needed in [`BlockPixelIteratorExt`].
+/// needed in [`PixelBlockIteratorExt`].
 ///
 /// # Metavariables
 ///
-/// * `$iter` - The iterator data. Should be a binding to [`BlockPixelIterator`]
+/// * `$iter` - The iterator data. Should be a binding to [`PixelBlockIterator`]
 /// * `$next_point` - The expression to use for returning the next point out of the iterator.
 /// * `$each_block` - The block of statements that gets run on each full block iteration.
-macro_rules! impl_blockpixeliterator {
+macro_rules! impl_pixelblockiterator {
     ($iter:ident, $next_point:expr, $each_block:block) => {
         {
             if $iter.y_block >= $iter.height {
@@ -58,7 +58,7 @@ macro_rules! impl_blockpixeliterator {
 ///
 /// It works by iterating through a block row by row, before moving on to the next block, which it
 /// also iterates through row by row until the end of the image.
-struct BlockPixelIterator {
+struct PixelBlockIterator {
     width: u32,
     height: u32,
     x_block_size: u32,
@@ -70,7 +70,7 @@ struct BlockPixelIterator {
     y: u32,
 }
 
-impl BlockPixelIterator {
+impl PixelBlockIterator {
     pub fn new(width: u32, height: u32, x_block_size: u32, y_block_size: u32) -> Self {
         Self {
             width,
@@ -86,42 +86,48 @@ impl BlockPixelIterator {
     }
 }
 
-impl Iterator for BlockPixelIterator {
+impl Iterator for PixelBlockIterator {
     type Item = (u32, u32);
 
     /// Iterates over each pixel, returning the x and y coordinate of the next pixel as a tuple.
     fn next(&mut self) -> Option<Self::Item> {
-        impl_blockpixeliterator!(self, (self.x_block + self.x, self.y_block + self.y), {})
+        impl_pixelblockiterator!(self, (self.x_block + self.x, self.y_block + self.y), {})
     }
 }
 
-/// See [`BlockPixelIterator`] for specifics on how this iterator works.
+/// See [`PixelBlockIterator`] for specifics on how this iterator works.
 ///
 /// This is an extension upon that iterator, that also returns the amount of blocks that have been
-/// processed thus far, which some encodings need.
-struct BlockPixelIteratorExt {
-    iterator: BlockPixelIterator,
+/// processed thus far, and the current column index (x coordinate) in the current block,
+/// which some encodings need.
+struct PixelBlockIteratorExt {
+    iterator: PixelBlockIterator,
     blocks: u32,
 }
 
-impl BlockPixelIteratorExt {
+impl PixelBlockIteratorExt {
     pub fn new(width: u32, height: u32, x_block_size: u32, y_block_size: u32) -> Self {
         Self {
-            iterator: BlockPixelIterator::new(width, height, x_block_size, y_block_size),
+            iterator: PixelBlockIterator::new(width, height, x_block_size, y_block_size),
             blocks: 0,
         }
     }
 }
 
-impl Iterator for BlockPixelIteratorExt {
-    type Item = (u32, u32, u32);
+impl Iterator for PixelBlockIteratorExt {
+    type Item = (u32, u32, u32, u32);
 
     /// Iterates over each pixel, returning the x and y coordinate of the next pixel as a tuple.
     fn next(&mut self) -> Option<Self::Item> {
         let iter = &mut self.iterator;
-        impl_blockpixeliterator!(
+        impl_pixelblockiterator!(
             iter,
-            (self.blocks, iter.x_block + iter.x, iter.y_block + iter.y),
+            (
+                self.blocks,
+                iter.x,
+                iter.x_block + iter.x,
+                iter.y_block + iter.y
+            ),
             {
                 self.blocks += 1;
             }
@@ -135,7 +141,7 @@ fn encode_pixels_rgb5a3(image: &RgbaImage) -> Vec<u8> {
     let dest_size = (width * height * 2).try_into().unwrap();
     let mut dest: Vec<u8> = Vec::with_capacity(dest_size);
 
-    for (x, y) in BlockPixelIterator::new(width, height, 4, 4) {
+    for (x, y) in PixelBlockIterator::new(width, height, 4, 4) {
         let p = image.get_pixel(x, y);
 
         let mut pixel: u16 = 0;
@@ -168,7 +174,7 @@ fn encode_pixels_argb8888(image: &RgbaImage) -> Vec<u8> {
 
     let mut dest_idx = 0;
 
-    for (block, x, y) in BlockPixelIteratorExt::new(width, height, 4, 4) {
+    for (block, _, x, y) in PixelBlockIteratorExt::new(width, height, 4, 4) {
         let p = image.get_pixel(x, y);
         let cur_idx = (block * 32) + dest_idx;
         let cur_dest_idx = cur_idx as usize;
@@ -179,6 +185,101 @@ fn encode_pixels_argb8888(image: &RgbaImage) -> Vec<u8> {
         dest[cur_dest_idx + 33] = p.0[2];
 
         dest_idx += 2;
+    }
+
+    dest
+}
+
+fn encode_pixels_rgb565(image: &RgbaImage) -> Vec<u8> {
+    let width = image.width();
+    let height = image.height();
+    let dest_size = (width * height * 2).try_into().unwrap();
+    let mut dest: Vec<u8> = Vec::with_capacity(dest_size);
+
+    for (x, y) in PixelBlockIterator::new(width, height, 4, 4) {
+        let p = image.get_pixel(x, y);
+
+        let mut pixel: u16 = 0x0000;
+        pixel |= ((p.0[0] >> 3) as u16) << 11;
+        pixel |= ((p.0[1] >> 2) as u16) << 5;
+        pixel |= (p.0[2] >> 3) as u16;
+
+        dest.push(((pixel >> 8) & 0xFF).try_into().unwrap());
+        dest.push((pixel & 0xFF).try_into().unwrap());
+    }
+
+    dest
+}
+
+fn encode_pixels_intensity_alpha4(image: &RgbaImage) -> Vec<u8> {
+    let width = image.width();
+    let height = image.height();
+    let dest_size = (width * height).try_into().unwrap();
+    let mut dest: Vec<u8> = Vec::with_capacity(dest_size);
+
+    for (x, y) in PixelBlockIterator::new(width, height, 8, 4) {
+        let p = image.get_pixel(x, y);
+
+        let mut pixel: u8 = 0;
+        pixel |= (((0.30 * p.0[0] as f32 + 0.59 * p.0[1] as f32 + 0.11 * p.0[2] as f32) * 15.
+            / 255.) as u8)
+            & 0xF;
+        pixel |= (((p.0[3] as f32 * 15. / 255.) as u8) & 0xF) << 4;
+
+        dest.push(pixel);
+    }
+
+    dest
+}
+
+fn encode_pixels_intensity_alpha8(image: &RgbaImage) -> Vec<u8> {
+    let width = image.width();
+    let height = image.height();
+    let dest_size = (width * height * 2).try_into().unwrap();
+    let mut dest: Vec<u8> = Vec::with_capacity(dest_size);
+
+    for (x, y) in PixelBlockIterator::new(width, height, 4, 4) {
+        let p = image.get_pixel(x, y);
+
+        let pixel = (0.30 * p.0[0] as f32 + 0.59 * p.0[1] as f32 + 0.11 * p.0[2] as f32) as u8;
+
+        dest.push(p.0[3]);
+        dest.push(pixel);
+    }
+
+    dest
+}
+
+fn encode_pixels_intensity_4(image: &RgbaImage) -> Vec<u8> {
+    let width = image.width();
+    let height = image.height();
+    let dest_size = (width * height / 2).try_into().unwrap();
+    let mut dest = vec![0u8; dest_size];
+
+    for (idx, (_, col, x, y)) in PixelBlockIteratorExt::new(width, height, 8, 8).enumerate() {
+        let p = image.get_pixel(x, y);
+
+        let pixel = ((0.30 * p.0[0] as f32 + 0.59 * p.0[1] as f32 + 0.11 * p.0[2] as f32) * 15.
+            / 255.) as u8;
+
+        dest[idx / 2] |= (pixel & 0xF) << ((!col & 0x1) * 4);
+    }
+
+    dest
+}
+
+fn encode_pixels_intensity_8(image: &RgbaImage) -> Vec<u8> {
+    let width = image.width();
+    let height = image.height();
+    let dest_size = (width * height).try_into().unwrap();
+    let mut dest: Vec<u8> = Vec::with_capacity(dest_size);
+
+    for (x, y) in PixelBlockIterator::new(width, height, 8, 4) {
+        let p = image.get_pixel(x, y);
+
+        let pixel = (0.30 * p.0[0] as f32 + 0.59 * p.0[1] as f32 + 0.11 * p.0[2] as f32) as u8;
+
+        dest.push(pixel);
     }
 
     dest
@@ -198,7 +299,7 @@ impl TextureEncoder {
             texture_type: TextureType::GCIX,
             pixel_format,
             data_format,
-            data_flags: DataFlags::None,
+            ..Default::default()
         }
     }
 
@@ -206,7 +307,6 @@ impl TextureEncoder {
         Self {
             texture_type: TextureType::GCIX,
             data_format,
-            data_flags: DataFlags::None,
             ..Default::default()
         }
     }
@@ -217,8 +317,13 @@ impl TextureEncoder {
         let rgba_img = img.into_rgba8();
 
         let encoded = match self.data_format {
+            DataFormat::Rgb565 => encode_pixels_rgb565(&rgba_img),
             DataFormat::Rgb5a3 => encode_pixels_rgb5a3(&rgba_img),
             DataFormat::Argb8888 => encode_pixels_argb8888(&rgba_img),
+            DataFormat::IntensityA4 => encode_pixels_intensity_alpha4(&rgba_img),
+            DataFormat::IntensityA8 => encode_pixels_intensity_alpha8(&rgba_img),
+            DataFormat::Intensity4 => encode_pixels_intensity_4(&rgba_img),
+            DataFormat::Intensity8 => encode_pixels_intensity_8(&rgba_img),
             _ => unimplemented!(),
         };
 
